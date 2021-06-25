892004e993de0327ee148d5a32eaf8cd9ebc99ed
jolikecat <65781195+jolikecat@users.noreply.github.com>
2021-06-25 16:57:03 +0900

    complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+    li.removeChild(completeButton);
+    li.removeChild(deleteButton);
+    li.appendChild(returnButton);
+    document.getElementById("complete-list").appendChild(li);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ（li）を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // button(戻す)タグ生成
+  const returnButton = document.createElement("button");
+  returnButton.innerText = "戻す";
+  returnButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ（li）を未完了リストから削除
+    returnFromCompleteList(returnButton.parentNode);
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
+    li.removeChild(returnButton);
+    document.getElementById("incomplete-list").appendChild(li);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(p);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定要素の削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 完了リストから未完了リストに戻す
+const returnFromCompleteList = (target) => {
+  document.getElementById("complete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
